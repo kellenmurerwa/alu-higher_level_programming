722a6147cf9cc8b54998ad10d20149658c9f22a1
Kells-design <kellenmurerwa724@gmail.com>
2024-02-28 00:05:31 -0800

    Task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i == 99:
-        print("{}".format(i))
-    else:
-        print("{:02}".format(i), end=", ")
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
