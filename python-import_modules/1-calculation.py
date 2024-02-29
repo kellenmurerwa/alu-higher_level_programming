bec4dc38fbe6277e11ac289f439064038e7836d4
Kells-design <kellenmurerwa724@gmail.com>
2024-02-29 02:30:31 -0800

    Task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,38 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
-    from calculator_1.py import add, sub, mul, div
-
-    def add(a,b):
-        a = 10
-        b = 5
-
-        return (a + b)
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-
-
-    def sub(a,b):
-        a = 10
-        b = 5
-
-        return (a - b)
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-
-
-    def mul(a,b):
-        a = 10
-        b = 5
-       
-        return (a * b)
-
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-
-
-    def div(a,b):
-        a = 10
-        b = 5
-
-        return int(a / b)
-    
     print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-
