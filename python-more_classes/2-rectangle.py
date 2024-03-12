633ab8e4eb7aadc2cf2a229559dcfa8ae5ae88f4
kellenmurerwa <kellenmurerwa724@gmail.com>
2024-03-13 00:08:00 +0200

    Task 1

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -38,14 +38,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     def area(self):
         return self.__width * self.__height
+
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return 2 * (self.__height + self.__width)
-
-
-
-
