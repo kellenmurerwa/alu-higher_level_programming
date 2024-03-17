7fc596d55ff41916e668899eabf0072fa5266615
kellenmurerwa <kellenmurerwa724@gmail.com>
2024-03-17 15:09:41 +0200

    Task 7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -54,8 +54,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         else:
-            rec = str(self.print_symbol * self.__width + "\n") * self.__height [:-1]
-            return rec
+            return (((str(self.print_symbol) * self.__width) + "\n") * self.__height)[:-1]
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
