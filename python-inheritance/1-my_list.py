e0396daca06e4b0d234110305801b24532381ed4
kellenmurerwa <kellenmurerwa724@gmail.com>
2024-03-17 19:21:31 +0200

    Tasks

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """First line of python"""
 
+
 class MyList(list):
+    """a child class inheriting from an inbuilt-class"""
     def print_sorted(self):
+        """Writing a sorting function"""
         print(sorted(self))
 
