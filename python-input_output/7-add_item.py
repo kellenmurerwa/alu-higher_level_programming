4d7ddc2087a949776f2a3d1a27a047427269c03c
kellenmurerwa <kellenmurerwa724@gmail.com>
2024-03-17 23:18:58 +0200

    Tasks

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,4 +12,5 @@ try:
     add_i = load_from_json_file('add_item.json')
     save_to_json_file(add_i + argv[1:], 'add_item.json')
 except Exception:
-    save_to_json_file(argv[1:], 'add_item.json')
+    save_to_json_file(argv[1:], 'add_item.json'
+
