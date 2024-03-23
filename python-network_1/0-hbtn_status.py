bcf0d429f69f6f24c6521378cd17c9e85c8166b1
Kells-design <kellenmurerwa724@gmail.com>
2024-03-23 12:06:45 -0700

    Task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-""" Pythonn script that fetches https://intranet.hbtn.io/status """
+""" Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    url = 'https://intranet.hbtn.io/status'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    req = urllib.request.Request(url, headers=headers)
+    with urllib.request.urlopen(req) as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
