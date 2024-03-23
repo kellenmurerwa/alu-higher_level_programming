14fd6820a6fc2139fcd39ffb839a702f1cccce05
Kells-design <kellenmurerwa724@gmail.com>
2024-03-23 09:59:27 -0700

    Task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ Pythonn script that fetches https://intranet.hbtn.io/status """
 import urllib.request
-
-
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
