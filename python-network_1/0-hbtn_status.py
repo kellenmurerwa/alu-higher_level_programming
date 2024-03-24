47f3482731eb37a3c38a035b25636dc52f81d066
Kells-design <kellenmurerwa724@gmail.com>
2024-03-24 10:16:25 -0700

    Task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,6 +3,8 @@
 import urllib.request
 if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
+    if url.startsWith("https://"):
+        url = 'https://alu-intranet.hbtn.io/status'
     headers = {'User-Agent': 'Mozilla/5.0'}
     req = urllib.request.Request(url, headers=headers)
     with urllib.request.urlopen(req) as response:
