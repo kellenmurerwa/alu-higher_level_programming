825a5017285d1716097d4923e051dd830b138250
Kells-design <kellenmurerwa724@gmail.com>
2024-03-24 10:28:27 -0700

    Task 0

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -6,6 +6,9 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
+    url = "https://intranet.hbtn.io/status"
+    if url.startswith("https://"):
+        url = "https://alu-intranet.hbtn.io/status"
+    r = requests.get(url)
     t = r.text
     print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
