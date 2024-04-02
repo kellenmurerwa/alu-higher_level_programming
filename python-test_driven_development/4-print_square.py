365af73ffbbed4a89c2d2e09db1bcaaf4312c834
Kells-design <kellenmurerwa724@gmail.com>
2024-04-02 03:48:52 -0700

    task 3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -17,6 +17,5 @@ def print_square(size):
         raise ValueError("size must be >= 0")
 
     for i in range(size):
-        for j in range(size):
-            print("# ", end="")
+        [print("#", end="") for j in range(size)]
         print("")
