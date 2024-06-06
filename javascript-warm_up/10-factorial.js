8698d0d6499f12c4fcc22569f1d5047597a3f8a8
kellenmurerwa <kellenmurerwa724@gmail.com>
2024-06-06 22:58:48 +0200

    JS warm-ups

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 function factorial (n) {
-    if (n === 1 || isNaN(n)) {
-        return 1;
-    }
+  if (n === 1 || isNaN(n)) {
+    return 1;
+  }
 
-    return n * factorial(n - 1);
+  return n * factorial(n - 1);
 }
 
 console.log(factorial(parseInt(process.argv[2])));
--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,8 +2,8 @@
 const argv = process.argv.slice(2);
 const output =
     argv.length > 0
-        ? argv.length === 1
-            ? 'Argument found'
-            : 'Arguments found'
-        : 'No argument';
+      ? argv.length === 1
+        ? 'Argument found'
+        : 'Arguments found'
+      : 'No argument';
 console.log(output);
